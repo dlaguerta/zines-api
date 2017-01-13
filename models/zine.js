f62758b6cdb7a3ed1cd331417254cf919c26fc2e
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-13 12:36:00 -0800

    adding model fields from flywheel zines

--- models/zine.js
+++ models/zine.js
@@ -3,6 +3,16 @@ var mongoose = require('mongoose'), Schema = mongoose.Schema;
 var zineSchema = new mongoose.Schema({
   title: 'string',
   creator: 'string',
+  sub_title: 'string',
+  volume: 'number',
+  number: 'number',
+  date: 'string',
+  circa_date: 'string',
+  place_of_publication: 'string',
+  tags: 'string',
+  contributor: 'string',
+  subject_genre: 'string',
+  press_house_publisher: 'string',
   library_id: [{ type: Schema.Types.ObjectId, ref: 'library' }]
 
 });
